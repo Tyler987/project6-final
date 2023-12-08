2508ec7d2ca7747c48589bc66ed5723ee5143b73
tyler <tylerkorth12@gmail.com>
2023-12-08 12:43:45 -0500

    s

--- public/review.js
+++ public/review.js
@@ -12,10 +12,10 @@ const showReviews = async () => {
     reviewsDiv.innerHTML = "";
 
     // Check if reviews is an array
-    if (!Array.isArray(reviews)) {
-        console.error("Invalid response from server:", reviews);
-        return;
-    }
+    // if (!Array.isArray(reviews)) {
+    //     console.error("Invalid response from server:", reviews);
+    //     return;
+    // }
 
     reviews.forEach((review) => {
         const section = document.createElement("section");
